8a207671c5f2699c596c670c894e2de7459a525e
jrdaynes <jrdaynes@gmail.com>
2020-02-19 11:38:51 -0700

    Created PortfolioContainer and updated app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
+import moment from "moment";
+
+import PortfolioContainer from './portfolio-container'
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
         <h1>DevCamp React Starter</h1>
-        <h2>React + Redux</h2>
+        <PortfolioContainer />
 
         <div style={{ color: "red" }}>
-          Warning: this is the experimental template, pardon the bugs
+          {moment().format("MMMM Do YYYY, h:mm:ss a")}
         </div>
       </div>
     );
